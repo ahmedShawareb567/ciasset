bb68fa68810c9cdd371170de578a3d582627b33a
Shawareb <ahmedshawareb567@gmail.com>
2022-11-13 13:51:25 +0200

    fix: delete staging

--- test/service.test.js
+++ test/service.test.js
@@ -7,7 +7,3 @@ test("1 + 2 to equal 3", () => {
 test("2 + 2 to equal 4", () => {
   expect(addAction(2, 2)).toBe(4);
 });
-
-test("2 + 3 to equal 5", () => {
-  expect(addAction(2, 3)).toBe(5);
-});
