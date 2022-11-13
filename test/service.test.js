b74f09b7790641bbb243439f2a14865d2c2f8b4d
Shawareb <ahmedshawareb567@gmail.com>
2022-11-13 13:21:52 +0200

    feat: branch staging

--- test/service.test.js
+++ test/service.test.js
@@ -3,3 +3,7 @@ const { addAction } = require("../src/service");
 test("1 + 2 to equal 3", () => {
   expect(addAction(1, 2)).toBe(3);
 });
+
+test("2 + 2 to equal 4", () => {
+  expect(addAction(2, 2)).toBe(4);
+});
